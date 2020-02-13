169b4e8536e4dd92568d4ba77f2f2f85e4226d51
Thijs Bruineman <tbruinem@f1r2s6.codam.nl>
2020-02-13 19:26:46 +0100

    added 8 side floodfill

--- src/ft_map_closecheck.c
+++ src/ft_map_closecheck.c
@@ -6,7 +6,7 @@
 /*   By: tbruinem <tbruinem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/05 19:14:11 by tbruinem       #+#    #+#                */
-/*   Updated: 2020/02/10 17:18:31 by tbruinem      ########   odam.nl         */
+/*   Updated: 2020/02/13 18:57:22 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,6 @@ void	ft_map_edit(t_data *data, char **reachable)
 
 int		ft_mapcheck(t_data *data, char **map, int x, int y)
 {
-	int l;
-	int r;
-	int u;
-	int d;
-
 	if (y < 0 || x < 0 || !map[y][x])
 	{
 		ft_map_del(map);
@@ -52,11 +47,14 @@ int		ft_mapcheck(t_data *data, char **map, int x, int y)
 		return (1);
 	if (map[y][x] != '1' && !ft_match("NSWE", map[y][x]))
 		map[y][x] = 'X';
-	l = ft_mapcheck(data, map, x - 1, y);
-	r = ft_mapcheck(data, map, x + 1, y);
-	u = ft_mapcheck(data, map, x, y - 1);
-	d = ft_mapcheck(data, map, x, y + 1);
-	if (l && r && u && d)
+	if (ft_mapcheck(data, map, x - 1, y) &&
+		ft_mapcheck(data, map, x + 1, y) &&
+		ft_mapcheck(data, map, x, y - 1) &&
+		ft_mapcheck(data, map, x, y + 1) &&
+		ft_mapcheck(data, map, x - 1, y - 1) &&
+		ft_mapcheck(data, map, x + 1, y + 1) &&
+		ft_mapcheck(data, map, x - 1, y + 1) &&
+		ft_mapcheck(data, map, x + 1, y - 1))
 		return (1);
 	ft_map_del(map);
 	exit(ft_error(data, ERR_STR_OPENMAP, -1));
